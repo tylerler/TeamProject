c8760e9fc4e87bf27d6442a8fb6be93d10b1d56c
harry <hwsantor@buffalo.edu>
2019-03-06 15:20:13 -0500

    newcommit

--- aa.py
+++ aa.py
@@ -1,26 +1,15 @@
-import bottle
 playerList = []
 
-
-@bottle.route('/join')
 def join(username):
     playerList.append(username)
 
-
-
-
-@bottle.route('/leave')
 def leave(username):
     for x in playerList:
         if x == username:
             playerList.remove(username)
-#test
-
-@bottle.route('/players')
 def players():
     return playerList
 
-
-
-
-bottle.run(host="0.0.0.0", port=8080, debug=True)
+print(join("hi"))
+print(leave("hi"))
+print(players())
