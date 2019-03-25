ac18dcaf4f467ec24479e19820d20ca26ff4bdbd
Anthony Morales <am348@buffalo,edu>
2019-03-25 14:06:24 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- src/GUI/NewGame.scala
+++ src/GUI/NewGame.scala
@@ -2,12 +2,6 @@ package GUI
 
 
 import javafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
-import scalafx.animation.AnimationTimer
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Polygon, Rectangle, Shape}
-import scalafx.scene.{Group, Scene}
 
 object NewGame extends JFXApp{
   var sceneGraphics: Group = new Group {}
@@ -15,61 +9,80 @@ object NewGame extends JFXApp{
   var playerY: Double = 600.0
   var playerSpeed: Double = 10
   var rotate: Double = 45
-  var curX: Double = 0.0
-  var curY: Double = 0.0
+  var windowWidth = 800
+  var windowHeight = 800
   var potX: Double = 0.0
   var potY: Double = 0.0
-//
+  var bullX: Double = 0
+  var bullY: Double = 0
 
- val player = Polygon (playerX, playerY, playerX - 40,playerY +80,playerX + 40,playerY + 80)
 
- sceneGraphics.children.add(player)
+  var count: Int = 0
+  var arr: Array[Array[Int]] = Array(Array(0,-1), Array(-1,-1),Array(-1,0),Array(-1,1),Array(0,1),Array(1,1),Array(1,0),Array(1,-1))
+  var allBullets: List[Shape] = List()
+
+
+  val player = Polygon (playerX, playerY, playerX - 40,playerY +80,playerX + 40,playerY + 80)
+
+  sceneGraphics.children.add(player)
 
   def keyPressed(keyCode: KeyCode): Unit = {
     keyCode.getName match {
-      case "D" => rotateR()
-    //  case "D" => player.rotate.value -= 10
+      case "A" => rotateL()
       case _ => println(keyCode.getName + " pressed with no action")
     }
   }
 
-  def keyPressed2(keyCode: KeyCode): Unit = {
-    keyCode.getName match {
-      case "A" => player.translateX.value += 20
-      case "D" => player.translateX.value -= 20
-      case _ => println(keyCode.getName + " pressed with no action")
+
+
+
+
+  def bullets(): Unit = {
+    val bullet = new Circle() {
+      centerX = 200
+      centerY = 200
+      radius = 20
     }
+    bullX = arr(count)(0)
+    bullY = arr(count)(1)
+
+
+    sceneGraphics.children.add(bullet)
+    allBullets = bullet :: allBullets
   }
-  def keyPressed3(keyCode: KeyCode): Unit = {
-    keyCode.getName match {
-      case "A" => player.translateY.value += 20
-      case "D" => player.translateY.value -= 20
-      case _ => println(keyCode.getName + " pressed with no action")
+
+  def rotateL(): Unit = {
+    count += 1
+    if(count == 8){
+      count = 0
     }
+    potX = arr(count)(0)
+    potY = arr(count)(1)
+    player.rotate.value -= 45
+
   }
-  ////
-
-
-  def rotateR(): Unit = {
-
-
-    }
 
 
 
   this.stage = new PrimaryStage {
     this.title = "2D Graphics"
-    scene = new Scene(800, 800) {
+    scene = new Scene(windowWidth, windowWidth) {
       content = List(sceneGraphics)
       addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed(event.getCode))
       //addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed2(event.getCode))
-      //addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed3(event.getCode))
 
     }
   }
 
 
   val update: Long => Unit = (time: Long) => {
+    player.translateX.value += potX
+    player.translateY.value += potY
+
+    for(bullet <- allBullets){
+      bullet.translateX.value+= 15
+      bullet.translateY.value += 15
+    }
 
 
   }
